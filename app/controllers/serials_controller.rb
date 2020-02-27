a765f528d69205785f99bc9f6eb825a2b42b7662
CornetS28 <corsam28@gmail.com>
2020-02-26 22:50:53 -0500

    modify serial controller

--- app/controllers/serials_controller.rb
+++ app/controllers/serials_controller.rb
@@ -2,18 +2,16 @@
 
 class SerialsController < ApplicationController
   before_action :set_serial, only: %i[show edit update destroy]
-  before_action :find_serials, only: %i[index show new edit]
-  before_action :authenticate_user!, except: %i[index show]
 
   def index
     @serials = Serial.all
-    @scenes = Scenes.all.order('created_at desc')
+    @scences = Scene.all.order('created_at desc')
   end
 
   def show
-    @scenes = Scene.where('serial_id = ?', @serial.id).order('created_at desc')
+    @scences = Scene.where('serial_id = ?', @serial.id).order('created_at desc')
     @serials = Serial.all
-    @scenes_ch_pagination = Scene.where('serial_id = ?', @serial.id).paginate(page: params[:page], per_page: 2)
+    @scences_ch_pagination = Scene.where('serial_id = ?', @serial.id).paginate(page: params[:page], per_page: 1)
   end
 
   def new
@@ -27,19 +25,19 @@ class SerialsController < ApplicationController
 
     respond_to do |format|
       if @serial.save
-        format.html { redirect_to @serial, notice: 'Vouz venez tout juste de creer une nouvelles serie!' }
+        format.html { redirect_to serials_path, notice: 'La serie a ete creee avec succes!' }
         format.json { render :show, status: :created, location: @serial }
       else
         format.html { render :new }
         format.json { render json: @serial.errors, status: :unprocessable_entity }
       end
     end
-  end
+ end
 
   def update
     respond_to do |format|
       if @serial.update(serial_params)
-        format.html { redirect_to @serial, notice: 'Cette a ete modifye avec succes!' }
+        format.html { redirect_to serials_path, notice: 'La serie a ete modifiee avec succes!' }
         format.json { render :show, status: :ok, location: @serial }
       else
         format.html { render :edit }
@@ -51,16 +49,12 @@ class SerialsController < ApplicationController
   def destroy
     @serial.destroy
     respond_to do |format|
-      format.html { redirect_to serials_url, notice: 'Vous aviez suprimer cette series avec succes!' }
+      format.html { redirect_to serials_url, notice: 'La serie a ete suprimee avec succes!' }
       format.json { head :no_content }
     end
   end
 
   private
-
-  def find_serials
-    @channels = Serial.all.order('created_at desc')
-  end
 
   def set_serial
     @serial = Serial.find(params[:id])
